a3c2b19e979bf53e6606526d4ec1354e510b5d3d
bregarulai <bregarulai@gmail.com>
2020-10-13 01:33:43 -0400

    Created home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import "./home.css";
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Home is working</h1>
+      <div className="container">
+        <h1>Samira TV Shows</h1>
+        <p>
+          A website where you can search for your favorite tv shows. You can
+          browse through all available shows or search for a particular show.
+          Click on any show to see more information about that show.
+        </p>
+      </div>
     </Layout>
   );
 };
